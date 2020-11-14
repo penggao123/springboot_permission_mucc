3aecc78e89b06d966a18a8a844d2eefd4d16c34e
langzi <1340852254@qq.com>
2020-11-14 13:18:03 +0800

    添加获取spring上下文中的bean的工具类 修改SpringBoot启动类并加载获取spring上下文工具类

--- src/main/java/com/mmall/SpringBootPermissionApplication.java
+++ src/main/java/com/mmall/SpringBootPermissionApplication.java
@@ -1,13 +1,19 @@
 package com.mmall;
 
+import com.mmall.utils.ApplicationContextHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootPermissionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPermissionApplication.class, args);
+		ApplicationContext run = SpringApplication.run(SpringBootPermissionApplication.class, args);
+
+		ApplicationContextHelper.setApplicationContext(run);
+
 	}
 
 
---
+++ src/main/java/com/mmall/utils/ApplicationContextHelper.java
@@ -0,0 +1,40 @@
+package com.mmall.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName ApplicationContextHelper
+ * @Description 获取spring上下文中的bean
+ * @Author gaopeng
+ * @Date 2020/11/14 12:53
+ * @Version 1.0
+ **/
+
+public class ApplicationContextHelper {
+
+    private static ApplicationContext applicationContext;
+
+    public static void setApplicationContext(ApplicationContext context) throws BeansException{
+        applicationContext = context;
+    }
+
+    public static <T> T popBean(Class<T> clazz){
+        if (applicationContext == null) {
+            return null;
+        }
+        return applicationContext.getBean(clazz);
+    }
+
+    public static <T> T popBean(String name, Class<T> clazz) {
+
+        if (applicationContext == null) {
+            return null;
+        }
+        return applicationContext.getBean(name,clazz );
+    }
+
+
+}
