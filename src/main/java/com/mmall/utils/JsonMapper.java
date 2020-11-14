7f303a9ab3e12917e4fda14c6607080723810923
langzi <1340852254@qq.com>
2020-11-14 12:47:44 +0800

    添加工具类(String转对象===对象转Sting)

---
+++ src/main/java/com/mmall/utils/JsonMapper.java
@@ -0,0 +1,56 @@
+package com.mmall.utils;
+
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.ser.impl.SimpleFilterProvider;
+import org.codehaus.jackson.type.TypeReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+  * @Author gaopeng
+  * @Description //String转对象  对象转String
+  * @Date 12:43 2020/11/14
+  * @param
+  * @return
+  **/
+public class JsonMapper {
+
+    private static final   Logger logger = LoggerFactory.getLogger(JsonMapper.class);
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        // config
+        objectMapper.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setFilters(new SimpleFilterProvider().setFailOnUnknownId(false));
+        objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_EMPTY);
+    }
+
+    public static <T> String obj2String(T src) {
+        if (src == null) {
+            return null;
+        }
+        try {
+            return src instanceof String ? (String) src : objectMapper.writeValueAsString(src);
+        } catch (Exception e) {
+            logger.warn("parse object to String exception, error:{}", e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String src, TypeReference<T> typeReference) {
+        if (src == null || typeReference == null) {
+            return null;
+        }
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? src : objectMapper.readValue(src, typeReference));
+        } catch (Exception e) {
+            logger.warn("parse String to Object exception, String:{}, TypeReference<T>:{}, error:{}", src, typeReference.getType(), e);
+            return null;
+        }
+    }
+}
